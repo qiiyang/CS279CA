70c359623d97ca74f9a6d1378afa365e273fe2d5
Q.I. Yang <qiyang@stanford.edu>
2015-09-08 16:46:48 -0700

    0.5: Done continuous example of convolution.

--- convolution/ex2.py
+++ convolution/ex2.py
@@ -2,60 +2,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # CONSTANTS
-HALF_RANGE = 5000    # Will use array[HALF_RANGE] as x=0, therefore arraysize = 2 * HALF_RANGE
-HALF_WINDOW = 500    # only plot between +/- this value, in order to remove the boundary effect
+HALF_RANGE = 5000    # Will use array[HALF_RANGE] as x=0, therefore arraysize = 2 * HALF_RANGE - 1
+PLOT_LIM = 500    # only plot between +/- this value, in order to remove the boundary effect
 
-def two_steps():
-    a = np.zeros(2*HALF_RANGE)
-    for i in range(200):
+def two_steps(width=200):
+    a = np.zeros(2*HALF_RANGE - 1)
+    for i in range(width):
         a[HALF_RANGE + i] = 1.
         a[HALF_RANGE - i - 1] = 2.
     return a
-    
+ 
+def gaussian(sigma=100):
+    a = 1. / sigma / (np.pi * 2)**0.5 * np.exp((np.arange(2*HALF_RANGE - 1) - HALF_RANGE)**2 / -2. / sigma**2)
+    return a
 
     
-def plot_f(f, style="dots", ylabel="y", title="", xlim=50.):
+def plot_f(f, fmt="b-", ylabel="y", title="", xlim=PLOT_LIM, ylims=None):
     plt.xlabel("x")
     plt.ylabel(ylabel)
     plt.title(title)
     plt.xlim(-xlim, xlim)
-    xs = np.arange(1000) - 500.
-    if style == "dots":
-        plt.plot(xs, f, "b.")
-    elif style == "stem":
-        plt.stem(xs, f, markerfmt="bo", linefmt="b--", basefmt="k-")
+    if ylims != None:
+        plt.ylim(ylims[0], ylims[1])
+    xs = np.arange(HALF_RANGE * 2 - 1) - HALF_RANGE
+    plt.plot(xs, f, fmt)
     plt.show()
     
-def plot_overlap():
-    f = two_spikes()
-    g = cone()
+def plot_convolution(f, g, fmt="b-", ylabel="y", title="", xlim=PLOT_LIM, ylims=None):
+    conv = np.convolve(f, g, "full")
     plt.xlabel("x")
-    plt.ylabel("y")
-    plt.title("g(x) spreading around each pixels of f(x)")
-    plt.xlim(-50, 50)
-    xs = np.arange(1000) - 500.
-    plt.plot(xs-20., g, "b.")
-    plt.plot(xs+20., 2*g, "b.")
-    plt.stem(xs, f, markerfmt="ro", linefmt="r--", basefmt="k-")
-    plt.show()
-    
-def plot_convolution(f, g):
-    g = cone()
-    conv = np.zeros(1000)
-    for i in range(-50, 50):
-        conv[500+i] = 2. * g[500+i - 20] + g[500+i + 20]
-
-    plt.xlabel("x")
-    plt.ylabel("y")
-    plt.title("g(x) convolves with f(x)")
-    plt.xlim(-50, 50)
-    xs = np.arange(1000) - 500.
-    plt.plot(xs, conv, "b.")
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.xlim(-xlim, xlim)
+    if ylims != None:
+        plt.ylim(ylims[0], ylims[1])
+    xs = np.arange(HALF_RANGE * 4 - 3) - HALF_RANGE * 2 - 1    
+    plt.plot(xs, conv, fmt)
     plt.show()
     
 if __name__ == '__main__':
-    #plot_f(two_spikes(), style="stem", title="f(x)")  # Plot the two spikes
-    #plot_f(cone(), title="g(x)")   # Plot the cone
-    #plot_overlap()
-    #plot_convolution()
+    #plot_f(two_steps(), fmt="b-", title="f(x)", ylims=(-0.3, 2.3))
+    #plot_f(gaussian(sigma=20.), title="g(x)")
+    plot_convolution(two_steps(), gaussian(20.), title="f convolves with g", ylims=(-0.3, 2.3))
     
